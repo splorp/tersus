ae9207a72303dd2cde5c2016ac6b3ad937eb63bc
Chris Harrison <chris@cdharrison.com>
2010-05-29 08:02:10 -0400

    Initial commit of basic search form template.

---
+++ searchform.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Simplicity
+ */
+?>
+
+<form method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
+  <div>
+    <input type="text" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="s" />
+    <input type="submit" id="searchsubmit" value="Search" />
+  </div>
+</form>
