456ca562718925197a31782b07a4d87f7e5a5d0f
Chris Harrison <chris@cdharrison.com>
2010-05-29 08:09:03 -0400

    Killed extra, unnecessary div.

--- searchform.php
+++ searchform.php
@@ -6,8 +6,6 @@
 ?>
 
 <form method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
-  <div>
     <input type="text" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="s" />
     <input type="submit" id="searchsubmit" value="Search" />
-  </div>
 </form>
