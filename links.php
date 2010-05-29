8056624c10f37bd10c6d3e2e4fa66cf192affbc5
Chris Harrison <chris@cdharrison.com>
2010-05-29 08:13:42 -0400

    Cleaned up formatting.

--- links.php
+++ links.php
@@ -11,9 +11,9 @@ Template Name: Links
 
 <?php get_header(); ?>
 
-<h2>Links:</h2>
-<ul>
-<?php wp_list_bookmarks(); ?>
-</ul>
+    <h2>Links:</h2>
+    <ul>
+        <?php wp_list_bookmarks(); ?>
+    </ul>
 
 <?php get_footer(); ?>
