b757603e3185709df294ce75f5d9c6d1faed4275
Chris Harrison <chris@cdharrison.com>
2010-05-31 16:35:53 -0400

    fixed tabbing issues.

--- header.php
+++ header.php
@@ -7,20 +7,19 @@
 
 <!DOCTYPE html>
 <html>
-
 	<head profile="http://gmpg.org/xfn/11">
 		<meta charset="<?php bloginfo('charset'); ?>" />
 		<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 		<!--[if IE]>
-        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]-->
+			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
 		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 		<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 		<?php wp_head(); ?>
 	</head>
-	
+
 	<body <?php body_class(); ?>>
-		
+
 		<h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
 		<p><?php bloginfo('description'); ?></p>
