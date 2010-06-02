0e2da5e9ac67e4ab857a92021c0cb5d3673e3e7d
splorp <grant@splorp.com>
2010-06-02 11:23:28 -0600

    Removed self-promotional WordPress comments Removed query timer calls Cleaned up the default WordPress and feed links

--- footer.php
+++ footer.php
@@ -4,12 +4,10 @@
  * @subpackage Simplicity
  */
 ?>
-
-		<!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way; it's our only promotion or advertising. -->
-		<p><?php bloginfo('name'); ?> is proudly powered by <a href="http://wordpress.org/">WordPress <?php bloginfo('version'); ?></a> <a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>. <!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. --></p>
+		<p>Powered by <a href="http://wordpress.org/">WordPress <?php bloginfo('version'); ?></a></p>
+		<p>Subscribe to <a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> or <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a></p>
 		
 		<?php wp_footer(); ?>
 
 	</body>
-
 </html>
