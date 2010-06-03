2b6bdfa1e25588b8dad779d52aea187b796d98d1
splorp <grant@splorp.com>
2010-06-02 19:58:53 -0600

    Removed profile attribute from head element Added link element for profile URI Reordered some of the head contents

--- header.php
+++ header.php
@@ -6,16 +6,17 @@
 ?>
 <!DOCTYPE html>
 <html>
-	<head profile="http://gmpg.org/xfn/11">
+	<head>
 		<meta charset="<?php bloginfo('charset'); ?>" />
 		<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+		<link rel="profile" href="http://gmpg.org/xfn/11" />
+		<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
+		<?php wp_head(); ?>
 		<!--[if IE]>
 			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
-		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-		<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
-		<?php wp_head(); ?>
 	</head>
 
 	<body>
