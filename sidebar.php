2776865d1d449ac6fadc5aaa1babc58665187078
splorp <grant@splorp.com>
2010-11-26 17:00:40 -0700

    Removed list formatting around search text

--- sidebar.php
+++ sidebar.php
@@ -14,17 +14,18 @@
 						get_search_form(); 
 
 						Because we’ve added searchform.php to the theme, using
-						get_search_form() isn't necessary here. But, in some
+						get_search_form() isn’t necessary here. But, in some
 						cases it may make more sense to use the built-in function:
 
 						http://codex.wordpress.org/Function_Reference/get_search_form
 					*/
 				?>
 			</li>
+		</ul>
 
 			<?php if ( is_404() || is_category() || is_day() || is_month() ||
 						is_year() || is_search() || is_paged() ) {
-			?> <li>
+			?>
 
 			<?php /* If this is a 404 page */ if (is_404()) { ?>
 			<?php /* If this is a category archive */ } elseif (is_category()) { ?>
@@ -46,14 +47,12 @@
 			<p>You have searched the <a href="<?php echo bloginfo('url'); ?>/"><?php echo bloginfo('name'); ?></a> blog archives
 			for <strong>'<?php the_search_query(); ?>'</strong>. If you are unable to find anything in these search results, you can try one of these links.</p>
 
-			<?php /* If this is a monthly archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
+			<?php /* Everything else */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
 			<p>You are currently browsing the <a href="<?php echo bloginfo('url'); ?>/"><?php echo bloginfo('name'); ?></a> blog archives.</p>
 
 			<?php } ?>
 
-			</li>
 		<?php }?>
-		</ul>
 		
 		<ul>
 			<?php wp_list_pages('title_li=<h2>Pages</h2>' ); ?>
