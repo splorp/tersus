ff93f5d993e6cd492c76ac8beeefa2af07f6d4d2
splorp <grant@splorp.com>
2010-12-01 09:18:51 -0700

    Consistent elseif syntax, please

--- archive.php
+++ archive.php
@@ -52,9 +52,9 @@
     
             if ( is_category() ) { // If this is a category archive
                 printf("<h2>Sorry, but there aren’t any posts in the %s category yet.</h2>", single_cat_title('',false));
-            } else if ( is_date() ) { // If this is a date archive
+            } elseif ( is_date() ) { // If this is a date archive
                 echo("<h2>Sorry, but there aren’t any posts with this date.</h2>");
-            } else if ( is_author() ) { // If this is a category archive
+            } elseif ( is_author() ) { // If this is a category archive
                 $userdata = get_userdatabylogin(get_query_var('author_name'));
                 printf("<h2>Sorry, but there aren’t any posts by %s yet.</h2>", $userdata->display_name);
             } else {
