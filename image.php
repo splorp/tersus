aeb8457f55dc9c4e2a256677a942aa2d0a1922cd
splorp <grant@splorp.com>
2010-12-30 12:09:22 -0700

    Replaced static class with post_class function

--- image.php
+++ image.php
@@ -8,7 +8,7 @@
 <?php get_header(); ?>
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<div class="post" id="post-<?php the_ID(); ?>">
+		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<h2><a href="<?php echo get_permalink($post->post_parent); ?>" rev="attachment"><?php echo get_the_title($post->post_parent); ?></a> &raquo; <?php the_title(); ?></h2>
 			<p><a href="<?php echo wp_get_attachment_url($post->ID); ?>"><?php echo wp_get_attachment_image( $post->ID, 'medium' ); ?></a></p>
 			<?php if ( !empty($post->post_excerpt) ) the_excerpt(); // this is the "caption" ?>
--- page.php
+++ page.php
@@ -8,7 +8,7 @@
 <?php get_header(); ?>
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-	<div class="post" id="post-<?php the_ID(); ?>">
+	<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
 		<h2><?php the_title(); ?></h2>
 		<?php the_content('<p>Read the rest of this page &raquo;</p>'); ?>
 		<?php wp_link_pages(array('before' => '<p>Pages: ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
