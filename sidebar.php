1c3b166442614608a91f64f388fc6c3bb69af53c
Chris Harrison <chris@cdharrison.com>
2011-08-19 11:45:37 -0400

    Added 2nd Dynamic Sidebar
    Added support for second dynamic sidebar. Cleaned up formatting.

--- sidebar.php
+++ sidebar.php
@@ -4,83 +4,118 @@
  * @subpackage Tersus
  */
 ?>
-		<ul>
-			<?php 	/* Widgetized sidebar, if you have the plugin installed. */
-				if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar() ) : ?>
-			<li>
-				<?php
-					include ( TEMPLATEPATH . '/searchform.php' );
-					/*
-						get_search_form(); 
 
-						Because we’ve added searchform.php to the theme, using
-						the built-in function isn’t necessary. We’ve left the 
-						syntax in place in case you prefer to use it instead.
+			<section class="sidebar" id="sidebar1">
 
-						http://codex.wordpress.org/Function_Reference/get_search_form
-					*/
-				?>
-			</li>
-		</ul>
-
-			<?php if ( is_404() || is_category() || is_day() || is_month() ||
-						is_year() || is_search() || is_paged() ) {
-			?>
-
-			<?php /* If this is a 404 page */ if (is_404()) { ?>
-			<?php /* If this is a category archive */ } elseif (is_category()) { ?>
-			<p>You are currently browsing the archives for the <?php single_cat_title(''); ?> category.</p>
-
-			<?php /* If this is a yearly archive */ } elseif (is_day()) { ?>
-			<p>You are currently browsing the <a href="<?php bloginfo('url'); ?>/"><?php echo bloginfo('name'); ?></a> blog archives
-			for the day <?php the_time('l, F jS, Y'); ?>.</p>
-
-			<?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
-			<p>You are currently browsing the <a href="<?php bloginfo('url'); ?>/"><?php echo bloginfo('name'); ?></a> blog archives
-			for <?php the_time('F, Y'); ?>.</p>
-
-			<?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
-			<p>You are currently browsing the <a href="<?php bloginfo('url'); ?>/"><?php echo bloginfo('name'); ?></a> blog archives
-			for the year <?php the_time('Y'); ?>.</p>
-
-			<?php /* If this is a monthly archive */ } elseif (is_search()) { ?>
-			<p>You have searched the <a href="<?php echo bloginfo('url'); ?>/"><?php echo bloginfo('name'); ?></a> blog archives
-			for <strong>'<?php the_search_query(); ?>'</strong>. If you are unable to find anything in these search results, you can try one of these links.</p>
-
-			<?php /* Everything else */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-			<p>You are currently browsing the <a href="<?php echo bloginfo('url'); ?>/"><?php echo bloginfo('name'); ?></a> blog archives.</p>
-
-			<?php } ?>
-
-		<?php }?>
-		
-		<ul>
-			<?php wp_list_pages('title_li=<h2>Pages</h2>' ); ?>
-			<li><h2>Archives</h2>
 				<ul>
-				<?php wp_get_archives('type=monthly'); ?>
+					<?php 	/* Widgetized sidebar, if you have the plugin installed. */
+						if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar( 'sidebar1' ) ) :
+					?>
+						<li>
+							<h2>Search the Site</h2>
+							<?php
+								include ( TEMPLATEPATH . '/searchform.php' );
+								/*
+									get_search_form(); 
+
+									Because we’ve added searchform.php to the theme, using
+									the built-in function isn’t necessary. We’ve left the 
+									syntax in place in case you prefer to use it instead.
+
+									http://codex.wordpress.org/Function_Reference/get_search_form
+								*/
+							?>
+						</li>
+
+						<?php if ( is_404() || is_category() || is_day() || is_month() || is_year() || is_search() || is_paged() ) { ?>
+							<li>
+
+								<?php if (is_404()) { /* If this is a 404 page */ ?>
+			
+								<?php } elseif (is_category()) { /* If this is a category archive */ ?>
+					
+									<p>You are currently browsing the archives for the <?php single_cat_title(''); ?> category.</p>
+
+								<?php } elseif (is_day()) { /* If this is a yearly archive */ ?>
+									
+									<p>You are currently browsing the <a href="<?php bloginfo('url'); ?>/"><?php echo bloginfo('name'); ?></a>
+									blog archives for the day <?php the_time('l, F jS, Y'); ?>.</p>
+
+								<?php } elseif (is_month()) { /* If this is a monthly archive */ ?>
+			
+									<p>You are currently browsing the <a href="<?php bloginfo('url'); ?>/"><?php echo bloginfo('name'); ?></a>
+									blog archives for <?php the_time('F, Y'); ?>.</p>
+
+								<?php } elseif (is_year()) { /* If this is a yearly archive */ ?>
+			
+									<p>You are currently browsing the <a href="<?php bloginfo('url'); ?>/"><?php echo bloginfo('name'); ?></a>
+									blog archives for the year <?php the_time('Y'); ?>.</p>
+
+								<?php } elseif (is_search()) { /* If this is a monthly archive */ ?>
+									
+									<p>You have searched the <a href="<?php echo bloginfo('url'); ?>/"><?php echo bloginfo('name'); ?></a>
+									blog archives for <strong>'<?php the_search_query(); ?>'</strong>. If you are unable to find anything in
+									these search results, you can try one of these links.</p>
+
+								<?php } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { /* Everything else */ ?>
+									
+									<p>You are currently browsing the <a href="<?php echo bloginfo('url'); ?>/"><?php echo bloginfo('name'); ?></a>
+									blog archives.</p>
+
+								<?php } ?>
+								
+							</li>
+							
+						<?php } ?>
+						
+						
+						<?php if ( is_home() || is_page() ) { /* If this is the frontpage */ ?>
+						
+							<?php wp_list_bookmarks(); ?>
+
+							<li>
+								<h2>Meta</h2>
+								<ul>
+									<?php wp_register(); ?>
+									<li><?php wp_loginout(); ?></li>
+									<li>Valid <a href="http://validator.w3.org/check/referer" title="This page validates as HTML5"><abbr title="HyperText Markup Language">HTML5</abbr></a></li>
+									<li>Valid <a href="http://jigsaw.w3.org/css-validator/validator?uri=<?php echo bloginfo('stylesheet_url'); ?>" title="The stylesheet for this page validates as CSS"><abbr title="Cascading Style Sheets">CSS</abbr></a></li>
+									<li>Valid <a href="http://validator.w3.org/feed/check.cgi?url=<?php echo bloginfo('rss_url'); ?>" title="The feed for this page validates as RSS"><abbr title="Really Simple Syndication">RSS</abbr></a></li>
+									<li><a href="http://gmpg.org/xfn/"><abbr title="XHTML Friends Network">XFN</abbr></a></li>
+									<?php wp_meta(); ?>
+								</ul>
+							</li>
+					
+						<?php } ?>
+				
+					<?php endif; ?>
+					
+					
+				
 				</ul>
-			</li>
-			<?php wp_list_categories('show_count=1&title_li=<h2>Categories</h2>'); ?>
-		</ul>
-		
-		<ul>
-			<?php /* If this is the frontpage */ if ( is_home() || is_page() ) { ?>
-				<?php wp_list_bookmarks(); ?>
-
-				<li>
-					<h2>Meta</h2>
-					<ul>
-						<?php wp_register(); ?>
-						<li><?php wp_loginout(); ?></li>
-						<li>Valid <a href="http://validator.w3.org/check/referer" title="This page validates as HTML5"><abbr title="HyperText Markup Language">HTML5</abbr></a></li>
-						<li>Valid <a href="http://jigsaw.w3.org/css-validator/validator?uri=<?php echo bloginfo('stylesheet_url'); ?>" title="The stylesheet for this page validates as CSS"><abbr title="Cascading Style Sheets">CSS</abbr></a></li>
-						<li>Valid <a href="http://validator.w3.org/feed/check.cgi?url=<?php echo bloginfo('rss_url'); ?>" title="The feed for this page validates as RSS"><abbr title="Really Simple Syndication">RSS</abbr></a></li>
-						<li><a href="http://gmpg.org/xfn/"><abbr title="XHTML Friends Network">XFN</abbr></a></li>
-						<?php wp_meta(); ?>
-					</ul>
-				</li>
-			<?php } ?>
-
-			<?php endif; ?>
-		</ul>
+			</section><!-- /#sidebar1 -->
+						
+			<section class="sidebar" id="sidebar2">
+				<ul>
+					<?php 	/* Widgetized sidebar, if you have the plugin installed. */
+						if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar( 'sidebar2' ) ) :
+					?>
+						<?php wp_list_pages('title_li=<h2>Pages</h2>' ); ?>
+						<li><h2>Archives</h2>
+							<ul>
+								<?php
+									// Displays a list of monthly archives.
+									wp_get_archives('type=monthly');
+								?>
+							</ul>
+						</li>
+	
+						<?php
+							// Displays a list of current blog categories.
+							wp_list_categories('show_count=1&title_li=<h2>Categories</h2>');
+						?>
+					<?php
+						endif;
+					?>
+				</ul>
+			</section>
