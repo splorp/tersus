0c3b2ed2636200067d6a20e248fd29ee5ad8fbf6
splorp <grant@splorp.com>
2011-08-19 16:56:30 -0600

    Added current theme style to footer

--- footer.php
+++ footer.php
@@ -4,9 +4,23 @@
  * @subpackage Tersus
  */
 ?>
+
+<?php
+
+	// This duplicates some code that exists in the
+	// header for handling the stylesheet switching.
+	// We might want to make this a single function
+	// or global elsewhere.
+
+	global $options;
+	foreach ($options as $value) {
+		if (get_settings( $value['id'] ) === FALSE) { $$value['id'] = $value['std']; } else { $$value['id'] = get_settings( $value['id'] ); }
+	}
+?>			
+
 		<footer>
 			<p>Powered by <a href="http://wordpress.org/" title="Code is poetry." rel="generator">WordPress <?php bloginfo('version'); ?></a></p>
-			<p>Themed by <a href="<?php echo THEME_URI; ?>" title="<?php echo THEME_DESCRIPTION; ?>" rel="generator"><?php echo THEME_NAME . ' ' . THEME_VERSION; ?></a></p>
+			<p>Themed by <a href="<?php echo THEME_URI; ?>" title="<?php echo THEME_DESCRIPTION; ?>" rel="generator"><?php echo THEME_NAME . ' ' . THEME_VERSION; ?></a> using the &#8220;<?php echo ($tersus_style_sheet); ?>&#8221; style</p>
 			<p>Subscribe to <a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> or <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a></p>
 			<?php wp_footer(); ?>
 		</footer>
