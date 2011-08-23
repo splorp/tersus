5716ec71424e1ffae68b2e0b25bec7f5007294db
Chris Harrison <chris@cdharrison.com>
2011-08-23 17:55:40 -0400

    Standardized stylesheet naming per request in Issue 20.

--- header.php
+++ header.php
@@ -22,22 +22,28 @@
 				<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style/css/layout.css" media="screen" />
 			<?php break; ?>	
 			<?php case "Super Ginormous":?>
-				<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style/css/custom-sg.css" media="screen" />
+				<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style/css/layout-superginormous.css" media="screen" />
 			<?php break; ?>
 			<?php case "Advanced":?>
-				<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style/css/advanced-layout.css" media="screen" />
+				<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style/css/layout-advanced.css" media="screen" />
 			<?php break; ?>
 		<?php } ?>
 		
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
 		<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		
 		<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 		<?php wp_head(); ?>
 		<!--[if IE]>
 			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
+		
+		<!-- My Stuff -->
+		
+		<meta name="y_key" content="fd82a55aff309824" />
+		<script src="/mint/?js" type="text/javascript"></script>
 		
 	</head>
 
@@ -48,6 +54,11 @@
 			// and provide a custom function which allows
 			// page-level ids or classes instead
 		?>
+		<section id="admin">
+			<h3><strong>What the heck is going on, you ask?</strong> Say hello to <a href="http://github.com/splorp/tersus/">Tersus</a>.</h3>
+			<p>Out of the box, Tersus provides only basic styling. Soon, it'll contain a few starter stylesheets (flavors?)
+			to assist in theme development.</p>
+		</section>
 		
 		<header>
 			<h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
