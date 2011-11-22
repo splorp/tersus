f7dfa6d26d3cea0ab2a3901a0897dd6b329a9b4c
Grant Hutchinson <splorp@mac.com>
2011-11-22 11:57:55 -0700

    Added section element and sidebar

--- image.php
+++ image.php
@@ -7,47 +7,50 @@
 
 <?php get_header(); ?>
 
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-			<h2><a href="<?php echo get_permalink($post->post_parent); ?>" rev="attachment"><?php echo get_the_title($post->post_parent); ?></a> : <?php the_title(); ?></h2>
-			<p><a href="<?php echo wp_get_attachment_url($post->ID); ?>"><?php echo wp_get_attachment_image( $post->ID, 'medium' ); ?></a></p>
-			<?php if ( !empty($post->post_excerpt) ) the_excerpt(); // this is the "caption" ?>
-			<?php the_content('<p>Read the rest of this entry</p>'); ?>
-			<?php previous_image_link() ?> | <?php next_image_link() ?>
+<section id="content">
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+		<h2><a href="<?php echo get_permalink($post->post_parent); ?>" rev="attachment"><?php echo get_the_title($post->post_parent); ?></a> : <?php the_title(); ?></h2>
+		<p><a href="<?php echo wp_get_attachment_url($post->ID); ?>"><?php echo wp_get_attachment_image( $post->ID, 'medium' ); ?></a></p>
+		<?php if ( !empty($post->post_excerpt) ) the_excerpt(); // this is the "caption" ?>
+		<?php the_content('<p>Read the rest of this entry</p>'); ?>
+		<?php previous_image_link() ?> | <?php next_image_link() ?>
 
-			<p>This entry was posted on <?php the_time('l, F jS, Y') ?> at <?php the_time() ?>.</p>
-			<p>Categories:</p>
-			<ul>
-				<li><?php the_category('</li><li>') ?></li>
-			</ul>
-			<?php the_taxonomies(); ?>
-			<p>You can follow any responses to this entry through the <?php post_comments_feed_link('RSS 2.0'); ?> feed.</p>
+		<p>This entry was posted on <?php the_time('l, F jS, Y') ?> at <?php the_time() ?>.</p>
+		<p>Categories:</p>
+		<ul>
+			<li><?php the_category('</li><li>') ?></li>
+		</ul>
+		<?php the_taxonomies(); ?>
+		<p>You can follow any responses to this entry through the <?php post_comments_feed_link('RSS 2.0'); ?> feed.</p>
 
-			<?php if ( comments_open() && pings_open() ) {
-			// Both Comments and Pings are open ?>
-			<p>You can <a href="#respond">leave a response</a>, or <a href="<?php trackback_url(); ?>" rel="trackback">trackback</a> from your own site.</p>
+		<?php if ( comments_open() && pings_open() ) {
+		// Both Comments and Pings are open ?>
+		<p>You can <a href="#respond">leave a response</a>, or <a href="<?php trackback_url(); ?>" rel="trackback">trackback</a> from your own site.</p>
 
-			<?php } elseif ( !comments_open() && pings_open() ) {
-			// Only Pings are Open ?>
-			<p>Responses are currently closed, but you can <a href="<?php trackback_url(); ?> " rel="trackback">trackback</a> from your own site.</p>
+		<?php } elseif ( !comments_open() && pings_open() ) {
+		// Only Pings are Open ?>
+		<p>Responses are currently closed, but you can <a href="<?php trackback_url(); ?> " rel="trackback">trackback</a> from your own site.</p>
 
-			<?php } elseif ( comments_open() && !pings_open() ) {
-			// Comments are open, Pings are not ?>
-			<p>You can skip to the end and leave a response. Pinging is currently not allowed.</p>
+		<?php } elseif ( comments_open() && !pings_open() ) {
+		// Comments are open, Pings are not ?>
+		<p>You can skip to the end and leave a response. Pinging is currently not allowed.</p>
 
-			<?php } elseif ( !comments_open() && !pings_open() ) {
-			// Neither Comments, nor Pings are open ?>
-			<p>Both comments and pings are currently closed.</p>
+		<?php } elseif ( !comments_open() && !pings_open() ) {
+		// Neither Comments, nor Pings are open ?>
+		<p>Both comments and pings are currently closed.</p>
 
-			<?php } edit_post_link('Edit','<p>','</p>'); ?>
-		</div>
+		<?php } edit_post_link('Edit','<p>','</p>'); ?>
+	</div>
 
-	<?php comments_template(); ?>
+<?php comments_template(); ?>
 
-	<?php endwhile; else: ?>
+<?php endwhile; else: ?>
 
-		<p>Sorry, no attachments matched your criteria.</p>
+	<p>Sorry, no attachments matched your criteria.</p>
 
 <?php endif; ?>
+</section>
 
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
