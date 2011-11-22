22c651c213f08ca48f775989ccbd39d3b501c4be
Grant Hutchinson <splorp@mac.com>
2011-11-22 11:57:13 -0700

    Added sidebar

--- links.php
+++ links.php
@@ -8,10 +8,11 @@
 <?php get_header(); ?>
 
 <section id="content">
-    <h2>Links</h2>
-    <ul>
-        <?php wp_list_bookmarks(); ?>
-    </ul>
+	<h2>Links</h2>
+	<ul>
+		<?php wp_list_bookmarks(); ?>
+	</ul>
 </section>
 
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
