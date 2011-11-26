4b30af7582ef910df39feb3a5d6653cf2b3b8f64
Grant Hutchinson <splorp@mac.com>
2011-11-26 09:36:59 -0700

    Validated rel attributes in footer

--- footer.php
+++ footer.php
@@ -19,9 +19,9 @@
 ?>			
 
 		<footer>
-			<p>Powered by <a href="http://wordpress.org/" title="Code is poetry." rel="generator">WordPress <?php bloginfo('version'); ?></a></p>
-			<p>Themed by <a href="<?php echo THEME_URI; ?>" title="<?php echo THEME_DESCRIPTION; ?>" rel="generator"><?php echo THEME_NAME . ' ' . THEME_VERSION; ?></a> using the &#8220;<?php echo ($tersus_style_sheet); ?>&#8221; style</p>
-			<p>Subscribe to <a href="<?php bloginfo('rss2_url'); ?>">Posts (RSS)</a> or <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a></p>
+			<p>Powered by <a href="http://wordpress.org/" title="Code is poetry.">WordPress <?php bloginfo('version'); ?></a></p>
+			<p>Themed by <a href="<?php echo THEME_URI; ?>" title="<?php echo THEME_DESCRIPTION; ?>"><?php echo THEME_NAME . ' ' . THEME_VERSION; ?></a> using the &#8220;<?php echo ($tersus_style_sheet); ?>&#8221; style</p>
+			<p>Subscribe to <a href="<?php bloginfo('rss2_url'); ?>" rel="alternate">Posts (RSS)</a> or <a href="<?php bloginfo('comments_rss2_url'); ?>" rel="alternate">Comments (RSS)</a></p>
 			<?php wp_footer(); ?>
 		</footer>
 	</body>
