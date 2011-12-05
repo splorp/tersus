248ffb99ed22b5ab6f4b54e71a776f89fca88c32
Grant Hutchinson <splorp@mac.com>
2011-12-05 15:12:25 -0700

    Nuked superfluous IDs from search form inputs

--- searchform.php
+++ searchform.php
@@ -6,6 +6,6 @@
 ?>
 
 <form method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
-    <input type="text" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="s" />
-    <input type="submit" id="searchsubmit" value="Search" />
+    <input type="text" value="<?php echo wp_specialchars($s, 1); ?>" name="s" />
+    <input type="submit" value="Search" />
 </form>
