53c28176dcf1967569a4b459440ca82cf8039278
Grant Hutchinson <splorp@mac.com>
2011-12-29 12:27:40 -0700

    Persnickety code formatting

--- category.php
+++ category.php
@@ -7,37 +7,31 @@
 
 <?php get_header(); ?>
 
-
 <section id="content">
 	<?php if (have_posts()) : ?>
-			<?php
-				$category_description = category_description();
-				if ( ! empty( $category_description ) )
-					echo apply_filters( 'category_archive_meta', '' . $category_description . '' );
-			?>
-			
-			<?php
-				$cat_ID = get_query_var('cat');
-				$children=  get_categories('child_of='.$cat_ID) ;
-				if ( $children && $children != 'No Children.' ) {
-					echo '<ul class="cat-children">';
-					echo '<li><strong>Sub-Categories:</strong></li>';
-						wp_list_categories('title_li&hide_empty=0&child_of='.$cat_ID);
-					echo '</ul>';
-				}
-			?>
 		<h2><?php printf( __( 'Category Archives: %s' ), '<span>' . single_cat_title( '', false ) . '</span>' ); ?></h2>
+		<?php
+			$category_description = category_description();
+			if ( ! empty( $category_description ) )
+				echo apply_filters( 'category_archive_meta', '' . $category_description . '' );
+			$cat_ID = get_query_var('cat');
+			$children=  get_categories('child_of='.$cat_ID) ;
+			if ( $children && $children != 'No Children.' ) {
+				echo '<ul class="cat-children">';
+				echo '<li><strong>Sub-Categories:</strong></li>';
+					wp_list_categories('title_li&hide_empty=0&child_of='.$cat_ID);
+				echo '</ul>';
+			}
+
+			/* Start the Loop */
 			
-		<?php /* Start the Loop */ ?>
-		<?php while ( have_posts() ) : the_post(); ?>
+			while ( have_posts() ) : the_post(); ?>
 		
 			<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 				<h3><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php if(the_title( '', '', false ) !='') the_title(); else echo 'Untitled';?></a></h3>
 				<?php the_excerpt('<p>Read the rest of this item</p>'); ?>
 				<p class="meta">This item was posted by <span class="vcard author"><cite class="fn"><a class="url" href="<?php the_author_meta('user_url') ?>"><?php the_author_meta('display_name'); ?></a></cite></span> on <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_time('c') ?>"><?php the_time('l, F jS, Y') ?></a>.</p>
-
 				<?php edit_post_link('Edit','<p>','</p>'); ?>
-				
 			</article>			
 					
 		<?php endwhile; ?>
--- tag.php
+++ tag.php
@@ -7,26 +7,23 @@
 
 <?php get_header(); ?>
 
-
 <section id="content">
 	<?php if (have_posts()) : ?>
-			<?php
-				$tag_description = tag_description();
-				if ( ! empty( $tag_description ) )
-					echo apply_filters( 'tag_archive_meta', '' . $tag_description . '' );
-			?>
-		<?php /* Start the Loop */ ?>
-		<?php while ( have_posts() ) : the_post(); ?>
 		<h2><?php printf( __( 'Tag Archives: %s' ), '<span>' . single_tag_title( '', false ) . '</span>' ); ?></h2>
+		<?php
+			$tag_description = tag_description();
+			if ( ! empty( $tag_description ) )
+				echo apply_filters( 'tag_archive_meta', '' . $tag_description . '' );
+
+			/* Start the Loop */
+			
+			while ( have_posts() ) : the_post(); ?>
 		
 			<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-
 				<h3><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php if(the_title( '', '', false ) !='') the_title(); else echo 'Untitled';?></a></h3>
 				<?php the_excerpt('<p>Read the rest of this item</p>'); ?>
 				<p class="meta">This item was posted by <span class="vcard author"><cite class="fn"><a class="url" href="<?php the_author_meta('user_url') ?>"><?php the_author_meta('display_name'); ?></a></cite></span> on <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_time('c') ?>"><?php the_time('l, F jS, Y') ?></a>.</p>
-
 				<?php edit_post_link('Edit','<p>','</p>'); ?>
-
 			</article>			
 					
 		<?php endwhile; ?>
