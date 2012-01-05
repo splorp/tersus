0f4309262d8565c0008d0d39f25ee1015a723786
Chris Harrison <cdharrison@gmail.com>
2012-01-05 12:33:50 -0500

    Added rough support for a generic fallback for /category/ (or whatever is set as the category_base.
    So far unable to get it to work for tag_base

--- 404.php
+++ 404.php
@@ -7,10 +7,23 @@
 
 <?php get_header(); ?>
 
-<section id="content">
-	<h2>Not found.</h2>
-	<p>Sorry, you seem to be looking for something that simply isn’t here.</p>
-</section>
+	<?php if ( dirname("/' . $category_base . '/") ) { ?>
+		
+		<section id="content">
+			<h2>Browse All Categories</h2>
+			<ul>
+				<?php wp_list_categories('style=list&title_li='); ?>
+			</ul>
+		</section>
+	
+	<?php } else { ?>
+		
+		<section id="content">
+			<h2>Not found.</h2>
+			<p>Sorry, you seem to be looking for something that simply isn’t here.</p>
+		</section>
+
+	<?php } ?> 
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
