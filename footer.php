306632c4b8b8e781f962f9caf5b82977b36fd308
Chris Harrison <cdharrison@gmail.com>
2012-04-10 00:11:36 -0400

    Simplified footer

--- footer.php
+++ footer.php
@@ -5,10 +5,14 @@
  */
 ?>
 		<footer>
-			<p>Powered by <a href="http://wordpress.org/" title="Code is poetry.">WordPress <?php bloginfo('version'); ?></a> &amp;
-				<a href="http://tersustheme.com" title="Tersus for WordPress" rel="external">Tersus</p>
-			<p>Subscribe to <a href="<?php bloginfo('rss2_url'); ?>" rel="alternate" title="Feed me.">Posts (RSS)</a> or
-				<a href="<?php bloginfo('comments_rss2_url'); ?>" rel="alternate" title="Feed me.">Comments (RSS)</a></p>
+			<p>
+				Powered by <a href="http://wordpress.org/" title="Code is poetry.">WordPress <?php bloginfo('version'); ?></a> &amp;
+				<a href="http://tersustheme.com" title="Tersus for WordPress" rel="external">Tersus</a>
+			</p>
+			<p>
+				Subscribe to <a href="<?php bloginfo('rss2_url'); ?>" rel="alternate" title="Feed me.">Posts (RSS)</a> or
+				<a href="<?php bloginfo('comments_rss2_url'); ?>" rel="alternate" title="Feed me.">Comments (RSS)</a>
+			</p>
 			<?php wp_footer(); ?>
 		</footer>
 	</body>
