fafd5460aa7b6a34c2a0073f457f0d0d12528ee4
Grant Hutchinson <grant@splorp.com>
2012-10-09 16:31:04 -0600

    Footer displays the current theme version again

--- footer.php
+++ footer.php
@@ -6,8 +6,8 @@
 ?>
 		<footer>
 			<p>
-				Powered by <a href="http://wordpress.org/" title="Code is poetry.">WordPress <?php bloginfo('version'); ?></a> &amp;
-				<a href="http://tersustheme.com" title="Tersus for WordPress" rel="external">Tersus</a>
+				Powered by <a href="http://wordpress.org/" rel="external" title="Code is poetry.">WordPress <?php bloginfo('version'); ?></a> &amp;
+				<a href="http://tersustheme.com/" rel="external" title="Simple, clean, cruftless.">Tersus <?php echo THEME_VERSION; ?></a>
 			</p>
 			<p>
 				Subscribe to <a href="<?php bloginfo('rss2_url'); ?>" rel="alternate" title="Feed me.">Posts (RSS)</a> or
