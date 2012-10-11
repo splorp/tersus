d1965c2490afcf72cfda8d84468ef9e49e61b4f0
Grant Hutchinson <grant@splorp.com>
2012-10-11 09:20:08 -0600

    Minor title edit

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
 		<footer>
 			<p>
 				Powered by <a href="http://wordpress.org/" rel="external" title="Code is poetry.">WordPress <?php bloginfo('version'); ?></a> &amp;
-				<a href="http://tersustheme.com/" rel="external" title="Simple, clean, cruftless.">Tersus <?php echo THEME_VERSION; ?></a>
+				<a href="http://tersustheme.com/" rel="external" title="Simple, cruftless themes.">Tersus <?php echo THEME_VERSION; ?></a>
 			</p>
 			<p>
 				Subscribe to <a href="<?php bloginfo('rss2_url'); ?>" rel="alternate" title="Feed me.">Posts (RSS)</a> or
