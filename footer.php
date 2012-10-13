09c9a8af493a584eaef32ac60adb245224a4c98e
Grant Hutchinson <grant@splorp.com>
2012-10-13 13:48:35 -0600

    Replace static values in theme information

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
 		<footer>
 			<p>
 				Powered by <a href="http://wordpress.org/" rel="external" title="Code is poetry.">WordPress <?php bloginfo('version'); ?></a><br />
-				Themed by <a href="http://tersustheme.com/" rel="external" title="Simple, cruftless themes.">Tersus <?php echo THEME_VERSION; ?></a>
+				Themed by <a href="<?php echo THEME_URI; ?>" rel="external" title="<?php echo THEME_DESCRIPTION; ?>"><?php echo THEME_NAME; ?> <?php echo THEME_VERSION; ?></a>
 				<?php if (defined('CHILD_THEME_NAME'))
 					{ ?><br />
 				Styled by <a href="<?php echo CHILD_THEME_URI; ?>" rel="external" title="<?php echo CHILD_THEME_DESCRIPTION; ?>"><?php echo CHILD_THEME_NAME; ?> <?php echo CHILD_THEME_VERSION; ?></a>
