ec24547604571d9c84cabc5e61d8693532e801d1
Grant Hutchinson <grant@splorp.com>
2013-11-02 14:35:38 -0600

    Option display of custom footer text, theme information formatted as list

--- footer.php
+++ footer.php
@@ -13,14 +13,18 @@
 ?>
 
 		<footer>
-			<?php if ($tersus_theme_information == "1"): ?>
-			<p>
-				Powered by <a href="http://wordpress.org/" rel="external" title="Code is poetry.">WordPress <?php bloginfo('version'); ?></a><br />
-				Themed by <a href="<?php echo THEME_URI; ?>" rel="external" title="<?php echo THEME_DESCRIPTION; ?>"><?php echo THEME_NAME; ?> <?php echo THEME_VERSION; ?></a>
-				<?php if (defined('CHILD_THEME_NAME')): ?><br />
-				Styled by <a href="<?php echo CHILD_THEME_URI; ?>" rel="external" title="<?php echo CHILD_THEME_DESCRIPTION; ?>"><?php echo CHILD_THEME_NAME; ?> <?php echo CHILD_THEME_VERSION; ?></a>
+			<?php
+			if ($tersus_footer_display == "1"):
+				echo (stripslashes($tersus_footer_text));
+			endif;
+			if ($tersus_theme_information == "1"): ?>
+			<ul>
+				<li>Powered by <a href="http://wordpress.org/" rel="external" title="Code is poetry.">WordPress <?php bloginfo('version'); ?></a></li>
+				<li>Themed by <a href="<?php echo THEME_URI; ?>" rel="external" title="<?php echo THEME_DESCRIPTION; ?>"><?php echo THEME_NAME; ?> <?php echo THEME_VERSION; ?></a></li>
+				<?php if (defined('CHILD_THEME_NAME')): ?>
+				<li>Styled by <a href="<?php echo CHILD_THEME_URI; ?>" rel="external" title="<?php echo CHILD_THEME_DESCRIPTION; ?>"><?php echo CHILD_THEME_NAME; ?> <?php echo CHILD_THEME_VERSION; ?></a></li>
 				<?php endif; ?>
-			</p>
+			</ul>
 			<?php endif; ?>
 			<p>
 				Subscribe to <a href="<?php bloginfo('rss2_url'); ?>" rel="alternate" title="Feed me.">Posts (RSS)</a> or
