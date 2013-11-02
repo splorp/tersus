ea82cdd017ff1557a9a682f3a6ae8b6bea20a217
Grant Hutchinson <grant@splorp.com>
2013-11-02 09:52:27 -0600

    Consolidated optional version information in footer

--- footer.php
+++ footer.php
@@ -13,15 +13,15 @@
 ?>
 
 		<footer>
+			<?php if ($tersus_theme_information == "1"): ?>
 			<p>
-				Powered by <a href="http://wordpress.org/" rel="external" title="Code is poetry.">WordPress <?php bloginfo('version'); ?></a>
-			<?php if ($tersus_theme_information == "1"): ?><br />
+				Powered by <a href="http://wordpress.org/" rel="external" title="Code is poetry.">WordPress <?php bloginfo('version'); ?></a><br />
 				Themed by <a href="<?php echo THEME_URI; ?>" rel="external" title="<?php echo THEME_DESCRIPTION; ?>"><?php echo THEME_NAME; ?> <?php echo THEME_VERSION; ?></a>
 				<?php if (defined('CHILD_THEME_NAME')): ?><br />
 				Styled by <a href="<?php echo CHILD_THEME_URI; ?>" rel="external" title="<?php echo CHILD_THEME_DESCRIPTION; ?>"><?php echo CHILD_THEME_NAME; ?> <?php echo CHILD_THEME_VERSION; ?></a>
 				<?php endif; ?>
-			<?php endif; ?>
 			</p>
+			<?php endif; ?>
 			<p>
 				Subscribe to <a href="<?php bloginfo('rss2_url'); ?>" rel="alternate" title="Feed me.">Posts (RSS)</a> or
 				<a href="<?php bloginfo('comments_rss2_url'); ?>" rel="alternate" title="Feed me.">Comments (RSS)</a>
