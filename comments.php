c83f2d5f5aa5e6745535759dee5fcb7e0f76d658
Grant Hutchinson <grant@splorp.com>
2014-01-03 22:05:49 -0700

    Removed weird comments template check

--- comments.php
+++ comments.php
@@ -7,10 +7,6 @@
 
 <?php
 
-// Do not delete these lines
-if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
-	die ('Please do not load this page directly. Thanks!');
-
 if ( post_password_required() ) { ?>
 	<p class="alert">This post is password protected. Enter the password to view comments.</p>
 <?php
