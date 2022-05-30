1c1c7ffe303ab4aafbb397cd89334fdd209ea158
Grant Hutchinson <grant@splorp.com>
2022-05-30 14:11:16 -0600

    Escaped home_url() functions

--- searchform.php
+++ searchform.php
@@ -5,7 +5,7 @@
  */
 ?>
 
-<form method="get" id="searchform" action="<?php echo home_url(); ?>/">
+<form method="get" id="searchform" action="<?php echo esc_url(home_url()); ?>/">
     <input type="text" value="<?php echo esc_html(the_search_query()); ?>" name="s" />
     <input type="submit" value="Search" />
 </form>
