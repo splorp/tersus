f4fa8dc23ca46420d4682f641b708652eb6f87d8
Grant Hutchinson <grant@splorp.com>
2022-06-05 13:54:24 -0600

    Removed wp_title from head

--- header.php
+++ header.php
@@ -8,7 +8,6 @@
 <html <?php language_attributes(); ?>>
 	<head>
 		<meta charset="<?php bloginfo('charset'); ?>" />
-		<title><?php wp_title('&mdash;', true, 'right'); ?></title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?2022060502" type="text/css" media="screen" />
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
