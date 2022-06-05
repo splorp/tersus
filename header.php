7e4346e136e9523be7b67373138cb311f1f216d4
Grant Hutchinson <grant@splorp.com>
2022-06-05 11:25:38 -0600

    Adjustments to cite and header selectors

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 		<meta charset="<?php bloginfo('charset'); ?>" />
 		<title><?php wp_title('&mdash;', true, 'right'); ?></title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?2022060300" type="text/css" media="screen" />
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?2022060502" type="text/css" media="screen" />
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
 		<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
