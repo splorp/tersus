a10eb2414baee1d1fbefc17386ca28758badbe80
Grant Hutchinson <grant@splorp.com>
2023-11-08 13:03:05 -0700

    Bumped datestamps

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 	<head>
 		<meta charset="<?php bloginfo('charset'); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?2023012700" type="text/css" media="screen" />
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?2023110800" type="text/css" media="screen" />
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 		<link rel="profile" href="https://gmpg.org/xfn/11" />
 		<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
